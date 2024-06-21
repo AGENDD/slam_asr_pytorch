9bb5d7a5097f2537a8ecedf52e49dd7ccb2d3da3
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-06-21 19:17:40 +0800

    st

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --mixed_precision fp16 finetune.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --mixed_precision fp16 finetune.py \
     --dataset temp_datasets/covost_en2zh-CN \
     --split clean \
     --output_dir ./output/covost_slam_asr \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --mixed_precisi
     --lr_scheduler_type linear \
     --source_max_len 16 \
     --target_max_len 512 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 4 \
     --max_steps 0 \
     --num_train_epochs 50 \
     --learning_rate 1e-4 \
