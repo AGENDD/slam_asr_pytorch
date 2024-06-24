9e4e6403443fff2a0498fa8aae15af9093b35450
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-06-24 16:17:15 +0800

    st

--- modeling/lora.py
+++ modeling/lora.py
@@ -2,23 +2,23 @@ import torch
 import torch.nn as nn
 
 class LoRALayer(nn.Module):
-    def __init__(self, in_dim, out_dim,rank, alpha=0.01):
+    def __init__(self, in_dim, out_dim,rank,device, alpha=0.01):
         super(LoRALayer, self).__init__()
         std_dev = 1 / torch.sqrt(torch.tensor(rank).float())
-        self.A = nn.Parameter(torch.randn(in_dim, rank)* std_dev)
-        self.B = nn.Parameter(torch.zeros(rank,out_dim))
-        self.alpha = alpha
+        self.A = nn.Parameter(torch.randn(in_dim, rank)* std_dev).to(device)
+        self.B = nn.Parameter(torch.zeros(rank,out_dim)).to(device)
+        self.alpha = alpha.to(device)
     def forward(self, x):
         return self.alpha * (x @ self.A @ self.B)
 
 class LinearWithLoRA(nn.Module):
-    def __init__(self, linear,rank, alpha=0.01):
+    def __init__(self, linear,rank,device, alpha=0.01):
         super(LinearWithLoRA, self).__init__()
         self.linear = linear
         for param in self.linear.parameters():
             param.requires_grad = False
             
-        self.lora = LoRALayer(linear.in_features, linear.out_features,rank, alpha)
+        self.lora = LoRALayer(linear.in_features, linear.out_features,rank,device, alpha)
 
     def forward(self, x):
         return self.linear(x) + self.lora(x)
