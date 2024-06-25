31a48f07540e0ac15db42b90482b0bd5c678ee34
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-06-25 14:59:19 +0800

    st

--- script2.sh
+++ script2.sh
@@ -31,3 +31,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 1 --mixed_precisi
     --trust_remote_code \
     --report_to tensorboard \
     --ddp_find_unused_parameters False \
+    --main_process_port 0 \
