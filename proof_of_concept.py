e6a59ff4db030d97f54321b04ae549bf81491219
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-06-26 14:25:20 +0800

    st

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -13,7 +13,7 @@ asr = SLAM_ASR(
     train_mode="adapter",
 )
 # load the state_dict from output/adapter_weights.pt
-adapter_weight = load_file("output/covost_slam_asr_ch2en/checkpoint-2600/model.safetensors")
+adapter_weight = load_file("output/covost_slam_asr_ch2en/checkpoint-2800/model.safetensors")
 asr.load_state_dict(adapter_weight, strict=False)
 
 
