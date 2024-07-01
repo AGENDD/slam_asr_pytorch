d5f759b58bf7aa8778f5c093be7914622a10705c
Jerry Jiang <83740943+AGENDD@users.noreply.github.com>
2024-07-01 14:07:12 +0800

    st

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -23,10 +23,10 @@ asr = SLAM_ASR(
 adapter_weight = load_file("output/rwkv/checkpoint-1200/model.safetensors")
 asr.load_state_dict(adapter_weight, strict=False)
 
-
+import resampy
 def map_to_array(batch):
-    
-    batch["speech"] = batch["audio"]['array']
+    audio_data_resampled = resampy.resample(batch["audio"]["array"], 48000, 16000)
+    batch["speech"] = audio_data_resampled
     batch['text'] = batch["sentence"]
     return batch
 
